28918f535c929a8b3e5ac2af7631101f337b6542
Jan-Hendrik Peters <nyanhp@users.noreply.github.com>
2019-12-12 12:43:05 +0100

    Added library

--- GpoToDsc/internal/scripts/preimport.ps1
+++ GpoToDsc/internal/scripts/preimport.ps1
@@ -2,22 +2,3 @@
 
 # Load the strings used in messages
 . Import-ModuleFile -Path "$($script:ModuleRoot)\internal\scripts\strings.ps1"
-
-class ValidationItem
-{
-    [string] $ValidationString
-    [string] $ConfigurationName
-    [string] $ValidationType
-
-    ValidationItem ([string] $valString, [string] $confName, [string] $valType)
-    {
-        $this.ValidationString = $valString
-        $this.ConfigurationName = $confName
-        $this.ValidationType = $valType
-    }
-
-    [string] ToString()
-    {
-        return $this.ValidationString
-    }
-}
