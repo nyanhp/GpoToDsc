1a030a5f8043475204cf4765ad08983dbf58bfa7
Jan-Hendrik Peters <nyanhp@users.noreply.github.com>
2019-12-12 11:09:10 +0100

    class added

--- GpoToDsc/tests/general/Help.Exceptions.ps1
+++ GpoToDsc/tests/general/Help.Exceptions.ps1
@@ -1,4 +1,22 @@
-﻿# List of functions that should be ignored
+﻿class ValidationItem
+{
+    [string] $ValidationString
+    [string] $ConfigurationName
+    [string] $ValidationType
+
+    ValidationItem ([string] $valString, [string] $confName, [string] $valType)
+    {
+        $this.ValidationString = $valString
+        $this.ConfigurationName = $confName
+        $this.ValidationType = $valType
+    }
+
+    [string] ToString()
+    {
+        return $this.ValidationString
+    }
+}
+# List of functions that should be ignored
 $global:FunctionHelpTestExceptions = @(
     
 )
